0c816518a7c7bf884294565f26da243f152260e6
MOHAMMED ELKADDOURI <kaddourim297@gmail.com>
2026-09-25 17:01:18 +0100

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/smiyt-lrepo-dyalk/',   // <-- bddl b smiyt repo dyalk f GitHub
   plugins: [react()],
   resolve: {
     alias: {
